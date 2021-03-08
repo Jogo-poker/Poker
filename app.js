681996261f55f4ad375d9ec34c069d64b2038c3a
CrossOv3r <allanhivdc@gmail.com>
2021-03-08 20:40:01 -0300

    Inicainado diretorio do usuario

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+//organizar, criar um diretorio proprio para colocar os render. (n sei bem onde criar pos isso não criei).
+
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
+	app.use(session({secret:'asgdkajsdhkajfhskajdhakshn'}));
+	app.use(bodyParser.urlencoded({extended:true}));
 	app.use(express.static(__dirname + '/public'));
 	app.use(express.static(__dirname + '/views'));
 
 	app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 	app.set('view engine', 'hbs');
+
+app.post('/Login', function(req, res){
+	var email = "teste@teste"; //simulando banco de dados
+	var senha = "123"; // simulando banco de dados
+	if(req.body.Email == email && req.body.Pass == senha ){
+		res.redirect('/');
+	}else{
+		res.render('Login');
+	}	
+});
 
 app.get('/', function(req, res){
 	res.render('Home');
@@ -17,6 +33,9 @@ app.get('/Jogar', function(req, res){
 });
 app.get('/Loja', function(req, res){
 	res.render('Loja');
+});
+app.get('/Conta', function(req, res){
+	res.render('Conta');
 });
 app.get('/Login', function(req, res){
 	res.render('Login');
