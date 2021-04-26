80feed2091f45fc43e944e054b6db073549bb24a
CrossOv3r <allanhivdc@gmail.com>
2021-04-26 09:58:57 -0300

    iniciando home

---
+++ public/JS/menu.js
@@ -0,0 +1,23 @@
+window.onload = () =>{
+    MenuRetratio();
+}
+
+function MenuRetratio(){
+    const menu = document.querySelector('.menuRetratio'); 
+    let AbrirMenuind = 0;
+    const click = document.querySelector('.option i');
+    click.addEventListener("click", () =>{
+        AbrirMenu(AbrirMenuind);
+        AbrirMenuind++;
+    })
+    function AbrirMenu(n) {
+        console.log(n);
+        const menu = document.querySelector('.menuRetratio');
+        // if(menu.style.display == 'block')
+        if(n % 2 == 0){
+            menu.style.display = "block";
+        }else if(n % 2 == 1){
+            menu.style.display = "none";
+        }
+    }
+}
